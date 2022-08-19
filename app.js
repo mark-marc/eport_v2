bd64dc37d1c11bc8f6fbb1b9c8406f81f76bad15
mark-marc <marcmedalle@hotmail.com>
2022-08-19 15:13:04 +1000

    added shapes to landing page and javascript to allow it to move when hover

--- app.js
+++ app.js
@@ -2,6 +2,20 @@
 //service_ggeohad
 //8Avufxh3TWCKqWNPK
 
+const scaleFactor = 1 / 20;
+
+function moveBackground(event){
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+    
+    for (let i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+    }
+}
+
 let contrastToggle = false;
 
 function toggleContrast() {
