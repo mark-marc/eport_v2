4e36755094056b49d0c1b344a6803402eb0d38c6
mark-marc <marcmedalle@hotmail.com>
2022-08-19 12:55:04 +1000

    Added javascript for contact section

---
+++ app.js
@@ -0,0 +1,29 @@
+//template_ah1eonc
+//service_ggeohad
+//8Avufxh3TWCKqWNPK
+
+function contact(){
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += " modal__overlay--visible";
+
+    emailjs
+    .sendForm(
+        'service_ggeohad',
+        'template_ah1eonc',
+        event.target,
+        '8Avufxh3TWCKqWNPK'
+
+    ).then(() => {
+        
+        loading.classList.remove( "modal__overlay--visible");
+        success.classList += " modal__overlay--visible";
+        
+    }).catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert (
+            "The email service is temporarily unavailable. Please contact me directly on markderickgo@gmail.com"
+        );
+    })
+}
